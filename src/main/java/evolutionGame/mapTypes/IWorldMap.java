a8ba3123c6bfe3d1336e442a5a41baa130ad4a3a
socz3qqq <soczawa@student.agh.edu.pl>
2022-12-28 23:39:24 +0100

    map structure and git test

--- src/main/java/evolutionGame/mapTypes/IWorldMap.java
+++ src/main/java/evolutionGame/mapTypes/IWorldMap.java
@@ -1,4 +1,12 @@
 package evolutionGame.mapTypes;
 
+import evolutionGame.mapElements.Animal;
+import evolutionGame.mapElements.Grass;
+import evolutionGame.mapElements.Vector2D;
+
 public interface IWorldMap {
+    void placeAnimal(Animal animal);
+    void placeGrass();
+    boolean isOccupied(Vector2D position);
+    Vector2D adjustMoveCoordinates(Vector2D position);
 }
