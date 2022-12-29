af3c5735800abae931a4224ae6f18e554bc96d4a
socz3qqq <soczawa@student.agh.edu.pl>
2022-12-29 22:52:07 +0100

    good map prototype

--- src/main/java/evolutionGame/Game.java
+++ src/main/java/evolutionGame/Game.java
@@ -1,8 +1,9 @@
 package evolutionGame;
 
-import evolutionGame.mapElements.MapDirections;
+import evolutionGame.mapTypes.Globe;
 
 public class Game {
     public static void main(String[] args){
+        SimulationEngine engine = new SimulationEngine(new Globe(5, 5), "Equator", "Random", "Predestination", 3, 4);
     }
 }
--- src/main/java/evolutionGame/mapTypes/IWorldMap.java
+++ src/main/java/evolutionGame/mapTypes/IWorldMap.java
@@ -1,12 +1,12 @@
 package evolutionGame.mapTypes;
 
 import evolutionGame.mapElements.Animal;
-import evolutionGame.mapElements.Grass;
 import evolutionGame.mapElements.Vector2D;
 
 public interface IWorldMap {
     void placeAnimal(Animal animal);
-    void placeGrass();
+    void placeAnimalRandomly();
+    void placeGrass(String grassType);
     boolean isOccupied(Vector2D position);
-    Vector2D adjustMoveCoordinates(Vector2D position);
+    public Vector2D adjustMoveCoordinates(Animal animal, Vector2D position);
 }
