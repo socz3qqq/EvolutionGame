4fa39f29a3f1faad9aaf336639f97a5082dbedd6
socz3qqq <100852273+socz3qqq@users.noreply.github.com>
2023-01-07 21:21:42 +0100

    Revert "Simulation works"
    This reverts commit a7aa2f3e761137665ba7d473f4e6c92ae4632969.

--- src/main/java/evolutionGame/mapElements/Animal.java
+++ src/main/java/evolutionGame/mapElements/Animal.java
@@ -3,95 +3,30 @@ package evolutionGame.mapElements;
 import evolutionGame.mapTypes.AbstractWorldMap;
 import evolutionGame.mapTypes.IWorldMap;
 
-import java.util.Objects;
 import java.util.Random;
 
 public class Animal {
+    private MapDirections currentDirection;
+    private Vector2D position;
+    private AbstractWorldMap map;
+    private int energy;
+    private int age;
+
+    private Genes genotype;
+
+    private int numberOfChild;
     private Random rand = new Random();
 
-    private MapDirections currentDirection = MapDirections.values()[rand.nextInt(8)];; //orientacja na mapie
-    private Vector2D position; //pozycja na mapie
-    private AbstractWorldMap map; //mapa
-    private int energy; //obecna energia
-    public int age = 0;
-    private int dayOfBirth = 0; //dzien narodzin
-    private int dayofDeath; //dzien smierci
-    private Genes genotype; //genom
-    private int numberOfChildren = 0; //liczba dzieci
-
-    //stałe globalne
-    private int minimalStuffedEnergy; //minimalna energia najedzenia
-    private int energyUsedForReproduction; //energia zuzyta na rozmnazanie
-    private int minChildMutation; //min liczba mutacji
-    private int maxChildMutation; //max liczba mutacji
-    private int genotypeLength; //dlugosc genomu
-
-
-    //    jeśli nie podamy pozycji zwierzaka, to pojawi się w losowym miejscu
-    public Animal(AbstractWorldMap map, int genotypeLength, int initialEnergy, int minimalStuffedEnergy, int energyUsedForReproduction, int minChildMutation, int maxChildMutation){
+    public Animal(AbstractWorldMap map, int genotypeLength, Vector2D initialPosition){
+        this.map = map;
+        this.position = initialPosition;
+        this.genotype = new Genes(genotypeLength);
+    }
+    public Animal(AbstractWorldMap map, int genotypeLength){
         this.map = map;
         this.position = new Vector2D(rand.nextInt(map.getMapWidth()), rand.nextInt(map.getMapHeight()));
         this.genotype = new Genes(genotypeLength);
-        this.energy = initialEnergy;
-        this.minimalStuffedEnergy = minimalStuffedEnergy;
-        this.energyUsedForReproduction = energyUsedForReproduction;
-        this.minChildMutation = minChildMutation;
-        this.maxChildMutation = maxChildMutation;
-        this.genotypeLength = genotypeLength;
     }
-
-    //szkielet Animala do tworzenia dziecka
-    public Animal(Vector2D position, AbstractWorldMap map, int dayOfBirth){
-        this.map = map;
-        this.position = position;
-        this.dayOfBirth = dayOfBirth;
-    }
-
-    public Animal copulate(Animal other, int currentDayOfSimulation){
-        if (this.energy >= minimalStuffedEnergy && other.energy >= minimalStuffedEnergy) {
-            Animal child = new Animal(this.position, this.map, currentDayOfSimulation);
-            this.numberOfChildren += 1;
-            other.numberOfChildren += 1;
-            this.decreaseEnergy(this.energyUsedForReproduction);
-            other.decreaseEnergy(this.energyUsedForReproduction);
-            child.minimalStuffedEnergy = this.minimalStuffedEnergy;
-            child.energyUsedForReproduction = this.energyUsedForReproduction;
-            child.minChildMutation = this.minChildMutation;
-            child.maxChildMutation = this.maxChildMutation;
-            child.energy = 2 * this.energyUsedForReproduction;
-
-            int numberOfThisGenes = (int) (this.energy/(this.energy + other.energy))*genotypeLength; //ile genow bierze od this
-            int numberOfOtherGenes = genotypeLength - numberOfThisGenes; //ile genow bierze od other
-            int[] childGenome = new int[genotypeLength]; //geny dziecka
-            int[] thisGenome = this.genotype.getGenes(); //geny this
-            int[] otherGenome = other.genotype.getGenes(); //geny other
-            int randomness = rand.nextInt(1);
-            switch (randomness){
-                //case 1: dziecko bierze od this z lewej strony
-                case 1 -> {
-                    for (int i=0; i < numberOfThisGenes; i++){
-                        childGenome[i] = thisGenome[i];
-                    }
-                    for (int i = genotypeLength-1; i>= numberOfThisGenes; i--){
-                        childGenome[i] = otherGenome[i];
-                    }
-                }
-                //case 0: dziecko bierze od this z prawej strony
-                case 0 -> {
-                    for (int i = genotypeLength - 1; i>=numberOfOtherGenes; i--){
-                        childGenome[i]=thisGenome[i];
-                    }
-                    for (int i=0; i<numberOfOtherGenes; i++){
-                        childGenome[i] = otherGenome[i];
-                    }
-                }
-            };
-            child.genotype = new Genes(childGenome, genotypeLength);
-            return child;
-        }
-        return null;
-    }
-
     public Animal(AbstractWorldMap map){
         this.map = map;
         this.position = new Vector2D(rand.nextInt(map.getMapWidth()), rand.nextInt(map.getMapHeight()));
@@ -109,38 +44,28 @@ public class Animal {
         return this.position.equals(position);
     }
 
-    public void move(String moveVariant, String cornerBehaviour){
+    public void move(String moveVariant){
         int rotation = genotype.getCurrentGene(moveVariant);
-        System.out.println(rotation + "  " + currentDirection);
-        this.currentDirection = this.currentDirection.rotate(rotation);
+        this.currentDirection.rotate(rotation);
 
         Vector2D displacementVector = this.currentDirection.toUnitVector();
         Vector2D newPosition = this.position.add(displacementVector);
-//        System.out.println(this.position + " chce przejść na "+ newPosition);
-
         //w zależności od rodzaju mapy ruch sie zmienia
-        this.position = this.map.adjustMoveCoordinates(this, newPosition, cornerBehaviour);
-//        System.out.println("po korekcie: " + this.position);
-        decreaseEnergy(1);
-        this.age += 1;
+        this.position = this.map.adjustMoveCoordinates(this, newPosition);
     }
     public int getEnergy(){
         return this.energy;
     }
-    public int getAge(){return  this.age;}
-    public int getNumberOfChildren(){return this.numberOfChildren;}
     public void increaseEnergy(int energy){
         this.energy += energy;
     }
     public void decreaseEnergy(int energy){
         this.energy -= energy;
     }
-    @Override
-    public String toString(){
-        return "A";
+    public void increaseEnergy(int energy){
+        this.energy += energy;
     }
-
-    public Genes getGenotype() {
-        return genotype;
+    public void decreaseEnergy(int energy){
+        this.energy -= energy;
     }
 }
--- src/main/java/evolutionGame/mapTypes/AbstractWorldMap.java
+++ src/main/java/evolutionGame/mapTypes/AbstractWorldMap.java
@@ -1,49 +1,28 @@
-// to do:
-// -zrobić zmianę pozycji zwierzęta poprzez obserwatora
-
-
-
 package evolutionGame.mapTypes;
 
-import evolutionGame.IPositionChangeObserver;
 import evolutionGame.mapElements.Animal;
 import evolutionGame.mapElements.Grass;
 import evolutionGame.mapElements.Vector2D;
-import evolutionGame.animalChallengerComparator;
 
 import java.util.*;
 
 
-abstract public class AbstractWorldMap implements IWorldMap, IPositionChangeObserver {
+abstract public class AbstractWorldMap implements IWorldMap{
     protected final Random rand = new Random();
     protected int mapWidth;
     protected int mapHeight;
     protected Vector2D upperBound;
     protected Vector2D lowerBound;
-    protected int energyFromGrass;
-    protected int animalCount;
-    protected List<Animal> deadAnimals;
+
+//    protected List<Animal> animals;
+    private List<Animal> deadAnimals;
     protected HashMap<Vector2D, List<Animal>> animals = new HashMap<>();
     protected HashMap<Vector2D, Grass> grass = new HashMap<>();
-    protected MapVisualizer mapVisualizer;
 
-    public AbstractWorldMap(int energyFromGrass){
-        this.mapVisualizer = new MapVisualizer(this);
-        this.deadAnimals = new ArrayList<>();
-        this.energyFromGrass = energyFromGrass;
-    }
-    @Override
-    public Object objectAt(Vector2D position) {
-        if(animals.getOrDefault(position, null) != null && !animals.get(position).isEmpty()){
-            return animals.get(position).get(0);}
-        return grass.getOrDefault(position, null);
-    }
-    @Override
-    public String toString() {
-        return mapVisualizer.draw(this.lowerBound, this.upperBound);
-    }
+    public AbstractWorldMap(){}
     @Override
     public void placeAnimal(Animal animal) {
+
         if(animals.containsKey(animal.getPosition())){
             List<Animal> animalsAtPosition = this.animals.get(animal.getPosition());
             animalsAtPosition.add(animal);
@@ -51,20 +30,24 @@ abstract public class AbstractWorldMap implements IWorldMap, IPositionChangeObse
             this.animals.put(animal.getPosition(), animalsAtPosition);
         }
         else{
-            List<Animal> newAnimals = new ArrayList<>();
-            newAnimals.add(animal);
-            this.animals.put(animal.getPosition(), newAnimals);
+            this.animals.put(animal.getPosition(), List.of(animal));
         }
-        this.animalCount += 1;
     }
-    public void placeGrass() {
-        Grass grass = new Grass(this, getGrassPosition());
+
+    @Override
+    public void placeGrass(String grassType) {
+        Grass grass = new Grass(this, grassType);
         this.grass.put(grass.getPosition(), grass);
     }
+
     @Override
     public boolean isOccupied(Vector2D position) {
         return this.animals.containsKey(position) || this.grass.containsKey(position);
     }
+    public boolean isOccupiedByGrass(Vector2D position){
+        return this.grass.containsKey(position);
+    }
+
     public int getMapHeight() {
         return mapHeight;
     }
@@ -72,182 +55,34 @@ abstract public class AbstractWorldMap implements IWorldMap, IPositionChangeObse
         return mapWidth;
     }
     public void removeDeadAnimals(){
-        if(this.animals.isEmpty()){
-            throw new RuntimeException("Zwierzęta się skończyły przed usuwaniem ich z mapy :(");
-        }
-        List<Animal> currentAnimals = new ArrayList<>();
-        for (Map.Entry<Vector2D, List<Animal>> entry : animals.entrySet()) {
-            currentAnimals.addAll(entry.getValue());
-        }
-        for(Animal being : currentAnimals){
-            if (being.getEnergy() <= 0) {
-                System.out.println(being);
-                removeAnimal(being);
+        Iterator<Map.Entry<Vector2D, List<Animal>>> iterator = animals.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<Vector2D, List<Animal>> entry = iterator.next();
+            for (Animal being : entry.getValue()){
+                if (being.getEnergy() == 0){
+                    entry.getValue().remove(being);
+                }
             }
         }
     }
     public void removeAnimal(Animal animal){
-        List<Animal> animalsAtPosition = this.animals.get(animal.getPosition());
-
-        this.animals.remove(animal.getPosition());
-        animalsAtPosition.remove(animal);
-        if(!animalsAtPosition.isEmpty())
-            this.animals.put(animal.getPosition(), animalsAtPosition);
-
-        System.out.println("zwierzak zdecchł :(, ostatnio widziano go na pozycji: "+ animal.getPosition());
+        this.animals.remove(animal);
         this.deadAnimals.add(animal);
-        this.animalCount -= 1;
     }
-    public void moveAllAnimals(String geneExecution, String cornerBehaviour){
-        if(this.animals.isEmpty()){
-            throw new RuntimeException("Zwierzęta się skończyły :(");
-        }
-        List<Animal> currentAnimals = new ArrayList<>();
-        for (Map.Entry<Vector2D, List<Animal>> entry : animals.entrySet()) {
-            currentAnimals.addAll(entry.getValue());
-        }
-        for(Animal being : currentAnimals) {
-            changeAnimalPosition(being, geneExecution, cornerBehaviour);
-            printAnimalInformation(being);
-
+    public void moveAllAnimals(String geneExecution){
+        for(Animal animal: animals){
+            animal.move(geneExecution);
         }
     }
     public void eatGrass(){
-        Set<Vector2D> currentGrassPositions = Set.copyOf(this.grass.keySet());
-        System.out.println(currentGrassPositions);
-        for(Vector2D grassPosition: currentGrassPositions){
+        for(Vector2D grassPosition: this.grass.keySet()){
+//           to może nie działać, bo nie wiem czy value w hashmapie jest tu przekazywane przez referencję
             List<Animal> challengerList = this.animals.get(grassPosition);
-
-            if(challengerList == null || challengerList.isEmpty()) {
-                continue;
-            }
-//          Sorting animal list according to:
-//          1) energy
-//          2) age
-//          3) number of children
-//          4) if the winner is still not found, the grass is eaten by random animal
-            challengerList.sort(new animalChallengerComparator());
             Animal winner = challengerList.get(0);
-//          the animal eats grass, and increases his energy
-            winner.increaseEnergy(this.energyFromGrass);
-            removeGrass(grassPosition);
-            System.out.println("grass was eaten! grass position: " + grassPosition);
+//            int maxEnergy = ;
+//            for(Animal challenger: challengerList){
+//                if( maxEnergy < )
+//            }
         }
     }
-    abstract public void removeGrass(Vector2D position);
-    public Vector2D adjustMoveCoordinates(Animal animal, Vector2D position, String cornerBehaviour) {
-        switch (cornerBehaviour) {
-            case "DevilishPost" -> {
-                if (!position.follows(lowerBound) || !position.precedes(upperBound)) {
-                    //            trzeba wybrać ile ma się zmieniać ta energia
-                    animal.decreaseEnergy(10);
-                    position = new Vector2D(rand.nextInt(mapWidth), rand.nextInt(mapHeight));
-                }
-                return position;
-            }
-            case "Globe" -> {
-                if (position.getX() < 0) position.setX(position.getX() + this.mapWidth);
-                if (position.getY() < 0) position.setY(position.getY() + this.mapHeight);
-                return new Vector2D(position.getX() % mapWidth, position.getY() % mapHeight);
-            }
-            default -> throw new IllegalArgumentException();
-        }
-    }
-    abstract public Vector2D getGrassPosition();
-
-    public void animalReproduction(int currentDayOfSimulation){
-        List<Animal> children = new ArrayList<>();
-        for (Map.Entry<Vector2D, List<Animal>> entry : animals.entrySet()) {
-            List<Animal> competitors = new ArrayList<>(List.<Animal>copyOf(entry.getValue()));
-            competitors.sort(new animalChallengerComparator());
-            for (int i = 0; i < competitors.size() - 1; i+=2) {
-                Animal childAnimal = competitors.get(i).copulate(competitors.get(i+1), currentDayOfSimulation);
-                if(childAnimal == null) break;
-                children.add(childAnimal);
-                System.out.println("Urodziło się nowe zwierzę! " + childAnimal.getPosition());
-            }
-        }
-        for(Animal child: children){
-            placeAnimal(child);
-            printAnimalInformation(child);
-        };
-    };
-    public void grassGrow(int count) {
-        for (int i = 0; i < count; i++) {
-            Vector2D newGrassPosition = getGrassPosition();
-            if(newGrassPosition != null){
-                Grass grass = new Grass(this, newGrassPosition);
-                this.grass.put(grass.getPosition(), grass);
-            }
-        }
-    }
-
-    private void changeAnimalPosition(Animal animal, String geneExecution, String cornerBehaviour){
-    if(!this.animals.get(animal.getPosition()).isEmpty()) {
-
-//      delete an animal from his current position
-        List<Animal> animalsAtCurrentPosition = this.animals.get(animal.getPosition());
-        animalsAtCurrentPosition.remove(animal);
-        this.animals.remove(animal.getPosition());
-        if(!animalsAtCurrentPosition.isEmpty())
-            this.animals.put(animal.getPosition(), animalsAtCurrentPosition);
-
-//      move the animal
-        animal.move(geneExecution, cornerBehaviour);
-
-//      add the animal to its new position
-        List<Animal> animalsAtNewPosition = this.animals.getOrDefault(animal.getPosition(), new ArrayList<>());
-        this.animals.remove(animal.getPosition());
-        animalsAtNewPosition.add(animal);
-        this.animals.put(animal.getPosition(), animalsAtNewPosition);
-
-    }
-    }
-// trzeba tego obserwera zaimplementować
-    @Override
-    public void positionChanged(Vector2D oldPosition, Vector2D newPosition){
-
-    }
-    public boolean areThereAnyAnimalsLeft(){
-        return !this.animals.isEmpty();
-    }
-    public static void printAnimalInformation(Animal animal){
-        System.out.println("pozycja: " + animal.getPosition());
-        System.out.println("energia: " + animal.getEnergy());
-        System.out.println("geny: " + Arrays.toString(animal.getGenotype().getGenes()));
-    }
-    public int getAnimalCount(){
-        return this.animalCount;
-    }
-    public int getGrassCount(){
-        return this.grass.size();
-    }
-    public int getUnoccupiedTilesCount(){
-        int count = 0;
-        for (int x = 0; x < mapWidth; x++) {
-            for (int y = 0; y < mapHeight; y++) {
-                if(!isOccupied(new Vector2D(x, y))) count += 1;
-            }
-        }
-        return count;
-    }
-    public int getAverageEnergyLevel(){
-        int allEnergy = 0;
-        for (Map.Entry<Vector2D, List<Animal>> entry : animals.entrySet()) {
-            for(Animal animal: entry.getValue()){
-                allEnergy += animal.getEnergy();
-            }
-
-        }
-        return allEnergy/this.animalCount;
-    }
-    public int getAverageLifeTime(){
-        if(deadAnimals.isEmpty()) return 0;
-        int lifeTimeSum = 0;
-        for(Animal deadAnimal: deadAnimals){
-            lifeTimeSum += deadAnimal.age;
-        }
-        return lifeTimeSum/deadAnimals.size();
-    }
-
 }
